270d8e14fcdec488ecee65e94eba836bc02a9c5e
Ben Kremer <git@benkremer.de>
2022-04-05 11:47:20 +0200

    🛍 added order items

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+
+    /**
+     * the related order
+     *
+     * @return BelongsTo
+     */
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    /**
+     * the related unit
+     *
+     * @return BelongsTo
+     */
+    public function unit(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class);
+    }
+}
---
+++ database/migrations/2022_04_05_095543_create_order_items_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Order;
+use App\Models\Unit;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(Order::class)->constrained('orders')->cascadeOnDelete();
+            $table->foreignIdFor(Unit::class)->constrained('units')->cascadeOnDelete();
+            $table->string('description');
+            $table->string('typology');
+            $table->unsignedFloat('amount');
+            $table->date('etd');
+            $table->unsignedFloat('price');
+            $table->unsignedFloat('provision');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
