f22242da531f44d643b5314056f87385c040f0e7
Ben Kremer <git@benkremer.de>
2022-07-05 21:25:32 +0200

    🚨 added policies

--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -11,7 +11,7 @@ class StoreOrderRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return false;
     }
@@ -21,10 +21,28 @@ class StoreOrderRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
-            //
+            'contract' => 'required|max:32',
+            'date' => 'required|date',
+            'seller.id' => 'exclude_if:seller.id,null|exists:companies,id|different:purchaser.id',
+            'seller.name' => 'exclude_unless:seller.id,null|required|min:3|max:255|different:purchaser.name',
+            'purchaser.id' => 'exclude_if:purchaser.id,null|exists:companies,id|different:seller.id',
+            'purchaser.name' => 'exclude_unless:purchaser.id,null|required|min:3|max:255|different:seller.name',
+            'agent.id' => 'sometimes|required|exists:users,id',
+            'order_items' => 'required|array|min:1',
+            'order_items.*.typology' => 'required',
+            'order_items.*.amount' => 'required|numeric',
+            'order_items.*.price' => 'required|numeric',
+            'order_items.*.provision' => 'required|numeric',
+            'order_items.*.unit.id' => 'required|exists:units,id',
+            'order_items.*.etd' => 'required|date',
+            'sub_agents' => 'sometimes|array',
+            'sub_agents.*.cut' => 'required|numeric',
+            'sub_agents.*.user' => 'required',
+            'sub_agents.*.user.id' => 'exclude_unless:sub_agents.*.user.name,null|required|exists:users,id|different:agent.id',
+            'sub_agents.*.user.name' => 'exclude_unless:sub_agents.*.user.id,null|required',
         ];
     }
 }
