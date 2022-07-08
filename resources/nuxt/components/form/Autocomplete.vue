0eb990ee40eea425ffcd3afbf92549ce46ab9cb2
Ben Kremer <git@benkremer.de>
2022-07-08 12:44:53 +0200

    📝 autocomplete component

---
+++ resources/nuxt/components/form/Autocomplete.vue
@@ -0,0 +1,127 @@
+<template>
+  <vee-validation-provider
+    v-slot="{ errors }"
+    :name="label"
+    :rules="rules"
+    :vid="name"
+    slim
+  >
+    <v-component
+      :is="tag"
+      v-model="localValue"
+      :error-messages="errors"
+      :items="entries"
+      :label="label"
+      :loading="isLoading"
+      :search-input.sync="search"
+      autocomplete="off"
+      hide-selected
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+      <template #selection="input">
+        <autocomplete-row
+          :return-object="$attrs['return-object'] !== undefined || $attrs['return-id'] !== undefined"
+          :avatars="avatars"
+          :input="input"
+        />
+      </template>
+      <template #item="input">
+        <autocomplete-row
+          :return-object="$attrs['return-object'] !== undefined || $attrs['return-id'] !== undefined"
+          :avatars="avatars"
+          :input="input"
+        />
+      </template>
+    </v-component>
+  </vee-validation-provider>
+</template>
+
+<script>
+import _ from 'lodash'
+import Input from './Input'
+import AutocompleteRow from '@/components/form/Autocomplete/Row'
+
+export default {
+  name: 'AutocompleteComponent',
+  components: {AutocompleteRow},
+  extends: Input,
+  inheritAttrs: true,
+  props: {
+    value: {
+      type: [Object, String, Number],
+      required: false,
+      default: () => null,
+    },
+    addable: {
+      type: Boolean,
+      default: () => false,
+    },
+    avatars: {
+      type: Boolean,
+      default: () => false,
+    },
+    entity: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default() {
+        return ''
+      },
+    },
+  },
+  data: () => ({
+    search: '',
+    isLoading: false,
+    entries: [],
+  }),
+  computed: {
+    tag() {
+      return this.addable ? 'v-combobox' : 'v-autocomplete'
+    },
+    localValue: {
+      get() {
+        return this.value
+      },
+      set(localValue) {
+        this.$emit('input', localValue)
+      },
+    },
+  },
+  watch: {
+    search(val) {
+      if (this.entries.length > 0) return
+      if (this.isLoading) return
+
+      this.isLoading = true
+
+      this.$axios(`/${this.$pluralize(this.entity)}`)
+        .then((res) => {
+          this.entries = res.data.data.map((item) =>
+            this.$attrs['return-object'] !== undefined || this.$attrs['return-id'] !== undefined
+              ? _.pick(item, 'id', 'name')
+              : item.name
+          )
+        })
+        .catch((err) => {
+          console.error(err)
+          this.$toast.error(err)
+        })
+        .finally(() => (this.isLoading = false))
+    },
+    value() {
+      const val =
+        typeof this.value === 'string' &&
+        this.$attrs['return-object'] !== undefined
+          ? {id: null, name: this.value}
+          : this.value
+
+      console.dir(val)
+
+      this.$emit('input', this.value)
+    },
+  },
+}
+</script>
---
+++ resources/nuxt/components/form/Autocomplete/Row.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-no-wrap text-truncate">
+    <app-avatar
+      v-if="avatars"
+      :name="returnObject ? input.item.name : input.item"
+      :size="20"
+      class="mr-2"
+    />
+    {{ returnObject ? input.item.name : input.item }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AutocompleteRow',
+  props: {
+    input: {
+      required: true,
+      type: Object,
+    },
+    returnObject: {
+      required: true,
+      type: Boolean,
+    },
+    avatars: {
+      type: Boolean,
+      default: () => false,
+    },
+  },
+}
+</script>
