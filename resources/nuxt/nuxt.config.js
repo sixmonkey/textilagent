bb098eb803ca6fc1d7831c59009e000301b51d1a
Ben Kremer <git@benkremer.de>
2022-07-08 12:48:23 +0200

    👷‍♀️ generate config

--- resources/nuxt/nuxt.config.js
+++ resources/nuxt/nuxt.config.js
@@ -9,23 +9,21 @@ export default {
     titleTemplate: '%s - textilexchange',
     title: 'textilexchange',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -86,6 +84,11 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
+  build: {},
+  generate: {
+    dir: 'public',
+    exclude: [
+      /^\/.+$/ // exclude anything but home (/) because we leave any routing to laravel
+    ]
   }
 }
